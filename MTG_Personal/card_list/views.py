ca1f3e3a91324ff7b40da92575f4f958a38f03c7
Eddie Medina <edmedina1990@gmail.com>
2022-05-25 14:03:08 -0500

    Added Django-filter for testing
    Be sure to check that it works properly

--- MTG_Personal/card_list/views.py
+++ MTG_Personal/card_list/views.py
@@ -10,7 +10,11 @@ def index(request):
 def card_search(request):
     if request.method == "POST":
         query_name = request.POST.get('name',None)
+        query_type = request.POST.get('card_type',None)
         if query_name:
             results = Card.objects.filter(name__contains=query_name)
+            return render(request,'card_list/card_search.html',{"results":results})
+        if query_type:
+            results = Card.objects.filter(name__contains=query_type)
             return render(request,'card_list/card_search.html',{"results":results})
     return render(request,'card_list/card_search.html')
