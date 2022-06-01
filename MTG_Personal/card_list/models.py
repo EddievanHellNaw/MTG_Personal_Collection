335a7f1d96b24b79e6c6ab2acd26e7db17b722e0
Eddie Medina <edmedina1990@gmail.com>
2022-06-01 12:37:52 -0500

    Esta wea ya mero queda lista

--- MTG_Personal/card_list/models.py
+++ MTG_Personal/card_list/models.py
@@ -4,15 +4,16 @@ from django.db import models
 class Card(models.Model):
     quantity = models.IntegerField(default=0)
     name = models.CharField(max_length=200)
-    effect = models.CharField(max_length=1000)
+    effect = models.CharField(max_length=1000, null=True)
     mana_value = models.IntegerField(default=0)
+    expansion_name = models.CharField(max_length=100, null=True)
     card_type = models.CharField(max_length=200, null=True)
     color = models.CharField(max_length=200, null=True)
     card_pic = models.ImageField(upload_to='media/', null=True)
     card_condition = models.CharField(max_length=100, null=True)
     foil = models.BooleanField(default=False)
     in_deck = models.BooleanField(default=False)
-    price = models.FloatField(default=0)
+    price = models.CharField(max_length=20, null=True)
 
     def __str__(self):
         return self.name
--- MTG_Personal/card_list/views.py
+++ MTG_Personal/card_list/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
 from .models import Card
+from selenium import webdriver                    
+from selenium.webdriver.common.by import By  
+from webdriver_manager.chrome import ChromeDriverManager
 
 
 # Create your views here.
 def index(request):
-    cards = Card.objects.all()
+    cards = Card.objects.exclude(name__exact='')
+    for each in cards:
+        get_card_price(each)
     return render(request, 'card_list/index.html', {'cards': cards})
 
 def card_search(request):
@@ -18,3 +23,62 @@ def card_search(request):
             results = Card.objects.filter(name__contains=query_type)
             return render(request,'card_list/card_search.html',{"results":results})
     return render(request,'card_list/card_search.html')
+
+def get_card_price(card):
+    
+    url = 'https://starcitygames.com/'
+
+    browser_options = webdriver.ChromeOptions()
+    browser_options.add_argument("--headless")
+    # browser = webdriver.Chrome(ChromeDriverManager().install())
+    browser = webdriver.Chrome(options=browser_options)
+    browser.get(url)
+    browser.implicitly_wait(20)
+    browser.maximize_window()
+
+    print("Finding card...")
+    print(card.name)
+    if card.name is not None:
+        try:
+            browser.find_element(By.CSS_SELECTOR, "[name='search_query']").send_keys(str(card.name))
+            search_button = browser.find_element(By.CLASS_NAME, "search-submit")
+            search_button.click()
+        except:
+            print("Name's not valid")
+            return
+
+    if card.foil:
+        print("Checking if Foil...")
+        try:
+            foil_select=browser.find_element(By.XPATH, '//*[@id="hawkfacet_finish"]/li[1]/a')
+            link = foil_select.get_attribute("href")
+            print("It's Foil")
+            browser.get(link)
+        except:
+            print("Element not interactable")
+            return
+    else:
+        try:
+            foil_select=browser.find_element(By.XPATH, '//*[@id="hawkfacet_finish"]/li[2]/a')
+            link = foil_select.get_attribute("href")
+            print("It's not foil")
+            browser.get(link)
+        except:
+            print("Something went wrong")
+            return            
+
+    cards = browser.find_elements(By.CLASS_NAME,"hawk-results-item")
+    
+    for each in cards:
+        c = each.text
+        price = each.find_element(By.XPATH, ".//descendant::div[contains(@class, 'price childAttributes')]")
+        if card.expansion_name in c and card.price != price.text:
+            card.price = price.text
+        
+
+    print(card.price)
+    card.save()
+    print("Price Updated")
+
+
+    browser.quit()
